6f9b4acce688c9d92845ff3f3830b42090b2fe44
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-09-04 13:54:29 -0400

    Update RandomNumbers.java

--- 01-JavaBasics/08-RandomNumbers/RandomNumbers.java
+++ 01-JavaBasics/08-RandomNumbers/RandomNumbers.java
@@ -3,10 +3,9 @@ import java.util.Random;
 public class RandomNumbers {
     public static void main(String[] args) {
         Random RandomNumber = new Random();
-        int rand = 0;
+        
         for (int i = 0; i < 100; i++) {
-            rand = RandomNumber.nextInt(10);
-            System.out.println("Random Number: " + rand);
+            System.out.println("Random Number: " + RandomNumber.nextInt(10));
         }
         
     }
