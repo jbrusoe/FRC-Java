871067b98aea770496896437679796b712b66659
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-10-16 09:53:04 -0400

    Rename directory

---
+++ 03-AdvancedJavaFeatures/02-Packages/PackageTest2.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class PackageTest2 {
+    public static void Test() {
+        System.out.println("This is my package!");
+      }
+
+      public static void main(String[] args) {
+        System.out.println("Test");
+      }
+}
