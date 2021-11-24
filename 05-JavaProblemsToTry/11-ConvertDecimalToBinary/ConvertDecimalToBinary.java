93354958b07fbc814105dc6abe88fe931902b200
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-11-24 16:26:59 -0500

    Update Java problems to try

--- 05-JavaProblemsToTry/11-ConvertDecimalToBinary/ConvertDecimalToBinary.java
+++ 05-JavaProblemsToTry/11-ConvertDecimalToBinary/ConvertDecimalToBinary.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class ConvertDecimalToBinary {
 
     public static void main(String[] args) {
-        int DecimalNumber = 512;
+        Scanner UserInput = new Scanner(System.in);
+        int[] BinaryValue = new int[10]; //implies max decimal value is 33554432
+        int BinaryIndex = 0;
+        int EnteredNumber = 9;
 
-        
+        System.out.print("\nEnter a positive integer: ");
+        EnteredNumber = UserInput.nextInt();
+        //UserInput.close();
+
+        System.out.println("Initial Number: " + EnteredNumber);
+
+        while (EnteredNumber > 0) {
+            BinaryValue[BinaryIndex] = EnteredNumber % 2;
+            BinaryIndex++;
+            EnteredNumber = EnteredNumber/2;
+        }
+
+        System.out.print("Binary Value: ");
+        for (int i = BinaryIndex -1 ; i >= 0; i--) {
+            System.out.print(BinaryValue[i] + " ");
+        }
+
+        System.out.println("");
     }
 }
