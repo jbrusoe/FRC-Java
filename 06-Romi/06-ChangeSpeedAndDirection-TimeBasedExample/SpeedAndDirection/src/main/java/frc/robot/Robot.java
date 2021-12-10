1fca797430e871cda1fa2144720b0fd2f6cec174
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-12-10 16:52:27 -0500

    Update Robot.java

--- 06-Romi/06-ChangeSpeedAndDirection-TimeBasedExample/SpeedAndDirection/src/main/java/frc/robot/Robot.java
+++ 06-Romi/06-ChangeSpeedAndDirection-TimeBasedExample/SpeedAndDirection/src/main/java/frc/robot/Robot.java
@@ -22,6 +22,8 @@ public class Robot extends TimedRobot {
 
   private final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
 
+  int runs = 0;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
