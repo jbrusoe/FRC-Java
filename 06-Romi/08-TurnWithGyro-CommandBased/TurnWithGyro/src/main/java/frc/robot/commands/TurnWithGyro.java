2e834285d56b1e26d258e763062512e51e163c60
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-12-22 16:39:41 -0500

    Update TurnWithGyro.java

--- 06-Romi/08-TurnWithGyro-CommandBased/TurnWithGyro/src/main/java/frc/robot/commands/TurnWithGyro.java
+++ 06-Romi/08-TurnWithGyro-CommandBased/TurnWithGyro/src/main/java/frc/robot/commands/TurnWithGyro.java
@@ -39,15 +39,20 @@ public class TurnWithGyro extends CommandBase {
     if (DT.getGyroAngleZ() <= TurnAngle) {
       DT.arcadeDrive(0, 0.5);
     }
+    else {
+      System.out.println("Turn Complete");
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    IO.setGreenLed(true);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return Math.abs(DT.getGyroAngleZ())>TurnAngle;
   }
 }
